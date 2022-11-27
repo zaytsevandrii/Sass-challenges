300c25c536844b080ad59fc1852c35996922f207
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-11-27 10:40:31 +0000

    pagination steps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import './App.scss';
+import { useState } from "react"
+import "./App.scss"
 
 function App() {
-  return (
-    <div className="app">
-     <h1>Project started</h1>
-     <h2>Hello</h2>
-     <h2>Hello</h2>
-    </div>
-  );
+  const [step,setStep] = useState(1)
+  const nextStep=()=>{
+    setStep(prev=>prev===4?prev=4:prev+1)
+  }
+  const prevStep=()=>{
+    setStep(prev=>prev===1?prev=1:prev-1)
+  }
+    return (
+        <div className="container">
+            <div className="progress-container">
+                <div className="progress" style={{width:`${((step-1)*33)}%`}}></div>
+                <div className="circle active">1</div>
+                <div className={`circle ${step>1&&'active'}`}>2</div>
+                <div className={`circle ${step>2&&'active'}`}>3</div>
+                <div className={`circle ${step>3&&'active'}`}>4</div>
+            </div>
+            <button  className="btn" disabled={step===1?true:false} onClick={prevStep}>
+                Prev
+            </button>
+            <button className="btn" disabled={step===4?true:false} onClick={nextStep}>Next</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
